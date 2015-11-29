d6a8fc902304a6e08941c9c8cbeddaa140ca1e61
vittoriomassaro <vittorio.massaro11@gmail.com>
2015-11-29 17:38:15 +0100

    first

---
+++ src/rubiks/ipl/Rubiks.java
@@ -0,0 +1,139 @@
+package rubiks.ipl;
+
+/**
+ * Parallel solver for rubik's cube puzzle.
+ * 
+ */
+
+public class Rubiks {
+
+	public static final boolean PRINT_SOLUTION = false;
+	public static final int SEQUENTIAL_LIMIT = 10;
+	/**
+	* Solves a Rubik's cube by iteratively searching for solutions with a
+	* greater depth. This guarantees the optimal solution is found. Repeats all
+	* work for the previous iteration each iteration though...
+	* 
+	* @param cube
+	*            the cube to solve
+	*/
+	private static void solve(Cube cube) {
+		// cache used for cube objects. Doing new Cube() for every move
+		// overloads the garbage collector
+		CubeCache cache = new CubeCache(cube.getSize());
+		int bound = 0;
+		int result = 0;
+
+		System.out.print("Bound now:");
+
+		while (result == 0) {
+		    bound++;
+		    cube.setBound(bound);
+
+		    System.out.print(" " + bound);
+		    result = solutions(cube, cache);
+		}
+
+		System.out.println();
+		System.out.println("Solving cube possible in " + result + " ways of "
+			+ bound + " steps");
+		}
+
+		public static void printUsage() {
+		System.out.println("Rubiks Cube solver");
+		System.out.println("");
+		System.out
+			.println("Does a number of random twists, then solves the rubiks cube with a simple");
+		System.out
+			.println(" brute-force approach. Can also take a file as input");
+		System.out.println("");
+		System.out.println("USAGE: Rubiks [OPTIONS]");
+		System.out.println("");
+		System.out.println("Options:");
+		System.out.println("--size SIZE\t\tSize of cube (default: 3)");
+		System.out
+			.println("--twists TWISTS\t\tNumber of random twists (default: 11)");
+		System.out
+			.println("--seed SEED\t\tSeed of random generator (default: 0");
+		System.out
+			.println("--threads THREADS\t\tNumber of threads to use (default: 1, other values not supported by sequential version)");
+		System.out.println("");
+		System.out
+			.println("--file FILE_NAME\t\tLoad cube from given file instead of generating it");
+		System.out.println("");
+	}
+
+	/**
+	* Main function.
+	* 
+	* @param arguments
+	*            list of arguments
+	*/
+	public static void main(String[] arguments) {
+		Cube cube = null;
+
+		// default parameters of puzzle
+		int size = 3;
+		int twists = 11;
+		int seed = 0;
+		String fileName = null;
+
+		// number of threads used to solve puzzle
+		// (not used in sequential version)
+
+		for (int i = 0; i < arguments.length; i++) {
+		    if (arguments[i].equalsIgnoreCase("--size")) {
+			i++;
+			size = Integer.parseInt(arguments[i]);
+		    } else if (arguments[i].equalsIgnoreCase("--twists")) {
+			i++;
+			twists = Integer.parseInt(arguments[i]);
+		    } else if (arguments[i].equalsIgnoreCase("--seed")) {
+			i++;
+			seed = Integer.parseInt(arguments[i]);
+		    } else if (arguments[i].equalsIgnoreCase("--file")) {
+			i++;
+			fileName = arguments[i];
+		    } else if (arguments[i].equalsIgnoreCase("--help") || arguments[i].equalsIgnoreCase("-h")) {
+			printUsage();
+			System.exit(0);
+		    } else {
+			System.err.println("unknown option : " + arguments[i]);
+			printUsage();
+			System.exit(1);
+		    }
+		}
+
+		// create cube
+		if (fileName == null) {
+		    cube = new Cube(size, twists, seed);
+		} else {
+		    try {
+			cube = new Cube(fileName);
+		    } catch (Exception e) {
+			System.err.println("Cannot load cube from file: " + e);
+			System.exit(1);
+		    }
+		}
+
+		// print cube info
+		System.out.println("Searching for solution for cube of size "
+			+ cube.getSize() + ", twists = " + twists + ", seed = " + seed);
+		cube.print(System.out);
+		System.out.flush();
+
+
+		// solve
+		long start = System.currentTimeMillis();
+		solve(cube);
+		long end = System.currentTimeMillis();
+
+		// NOTE: this is printed to standard error! The rest of the output is
+		// constant for each set of parameters. Printing this to standard error
+		// makes the output of standard out comparable with "diff"
+		System.err.println("Solving cube took " + (end - start)
+			+ " milliseconds");
+
+	}
+
+}
