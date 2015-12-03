9f8afe4f3061ab2e8f86d8191203c3465c3c7693
vittoriomassaro <vittorio.massaro11@gmail.com>
2015-12-03 15:51:18 +0100

    seq lim

--- src/rubiks/ipl/Master.java
+++ src/rubiks/ipl/Master.java
@@ -160,6 +160,7 @@ public class Master implements MessageUpcall{
         
         if ( ! slaves.isEmpty() &&  ! (cube.getTwists() < INITIAL_ITERATION) && !(cube.getBound() - cube.getTwists() < 3))
     	{
+        	System.err.println("send a cube.");
     		sendCube(cube);
     		return 0;
     	}
@@ -220,6 +221,7 @@ public class Master implements MessageUpcall{
 		/*
 		 * 
 		 */
+		System.err.println("result back.");
 		ResultMessage resultMessage = (ResultMessage) message.readObject();
 		message.finish();
 		
