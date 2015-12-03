0f010cdfb98ffb2a0a42ca94889805d83d9642f8
vittoriomassaro <vittorio.massaro11@gmail.com>
2015-12-03 16:27:33 +0100

    seq lim

--- src/rubiks/ipl/Master.java
+++ src/rubiks/ipl/Master.java
@@ -10,7 +10,7 @@ import ibis.ipl.*;
 public class Master{
 	
 	static final int INITIAL_ITERATION = 2;
-	static final int SEQUENTIAL_THRESHOLD = 0;
+	static final int SEQUENTIAL_THRESHOLD = 4;
 	Ibis myIbis;
 	Cube cube;
 	PortType masterToSlavePortType;
